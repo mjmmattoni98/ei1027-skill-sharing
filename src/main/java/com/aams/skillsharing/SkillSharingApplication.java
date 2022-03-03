db22ba52f65d90dbde6d73856929799319343454
marcos <javier.magni@gmail.com>
2022-03-03 15:50:27 +0100

    first commit

---
+++ src/main/java/com/aams/skillsharing/SkillSharingApplication.java
@@ -0,0 +1,14 @@
+package com.aams.skillsharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillSharingApplication {
+//    private static final Logger log = Logger.getLogger(SkillSharingApplication.class.getName());
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkillSharingApplication.class, args);
+    }
+
+}
