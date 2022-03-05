a965c425860929ef7129207e0a61ae09d621daac
Alejandro Guinot <al375927@uji.es>
2022-03-05 12:03:21 +0100

    Controladores (faltan imports DAOs)

---
+++ src/main/java/com/aams/skillsharing/controller/CollaborationController.java
@@ -0,0 +1,147 @@
+package com.aams.skillsharing.controller;
+
+import com.aams.skillsharing.model.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+import java.time.LocalDate;
+import java.util.LinkedList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/collaboration")
+public class CollaborationController extends RoleController{
+    private CollaborationDao collaborationDao;
+    private OfferDao offerDao;
+    private RequestDao requestDao;
+    private StudentDao studentDao;
+    private EmailDao emailDao;
+    private static final CollaborationValidator validator = new CollaborationValidator();
+
+    @Autowired
+    public void setCollaborationDao(CollaborationDao collaborationDao) {
+        this.collaborationDao = collaborationDao;
+    }
+
+    @Autowired
+    public void setEmailDao(EmailDao emailDao) {
+        this.emailDao = emailDao;
+    }
+
+    @Autowired
+    public void setOfferDao(OfferDao offerDao) {
+        this.offerDao = offerDao;
+    }
+
+    @Autowired
+    public void setRequestDao(RequestDao requestDao) {
+        this.requestDao = requestDao;
+    }
+
+    @Autowired
+    public void setStudentDao(StudentDao studentDao) {
+        this.studentDao = studentDao;
+    }
+
+    @RequestMapping("/list")
+    public String listCollaborations(HttpSession session, Model model) {
+        InternalUser user = checkSession(session, SKP_ROLE);
+        if (user == null){
+            model.addAttribute("user", new InternalUser());
+            return "login";
+        }
+
+        model.addAttribute("collaborations", collaborationDao.getCollaborations());
+        return "collaboration/list";
+    }
+
+    @RequestMapping("/list/{username}")
+    public String listCollaborationsStudent(HttpSession session, Model model, @PathVariable String username) {
+        if (session.getAttribute("user") == null){
+            model.addAttribute("user", new InternalUser());
+            return "login";
+        }
+
+        model.addAttribute("collaborations", collaborationDao.getCollaborationsStudent(username));
+        model.addAttribute("student", username);
+        return "collaboration/list";
+    }
+
+    @RequestMapping(value = "/add/{idOffer}/{idRequest}")
+    public String addCollaboration(HttpSession session, Model model, @PathVariable int idOffer, @PathVariable int idRequest) {
+        if (session.getAttribute("user") == null){
+            model.addAttribute("user", new InternalUser());
+            return "login";
+        }
+
+        Collaboration collaboration = new Collaboration();
+        collaboration.setIdOffer(idOffer);
+        collaboration.setIdRequest(idRequest);
+        model.addAttribute("collaboration", collaboration);
+        return "collaboration/add";
+    }
+
+    @PostMapping(value = "/add")
+    public String processAddCollaboration(@ModelAttribute("collaboration") Collaboration collaboration,
+                                          BindingResult bindingResult) {
+        validator.validate(collaboration, bindingResult);
+        if (bindingResult.hasErrors()) {
+            return "collaboration/add";
+        }
+        try {
+            collaborationDao.addCollaboration(collaboration);
+            Student student = studentDao.getStudent(offerDao.getOffer(collaboration.getIdOffer()).getUsername());
+            Email email = new Email();
+            email.setSender("skill.sharing@uji.es");
+            email.setReceiver(student.getEmail());
+            email.setSendDate(LocalDate.now());
+            email.setSubject("New collaboration");
+            email.setBody("You have a new collaboration with " + requestDao.getRequest(collaboration.getIdRequest()).getUsername());
+            emailDao.addEmail(email);
+        }
+        catch (DataAccessException e){
+            throw new SkillSharingException("Error accessing the database\n" + e.getMessage(),
+                    "ErrorAccessingDatabase", "/");
+        }
+        return "redirect:list/";
+    }
+
+    @GetMapping(value = "/update/{idOffer}/{idRequest}")
+    public String updateCollaboration(HttpSession session, Model model, @PathVariable int idOffer, @PathVariable int idRequest) {
+        if (session.getAttribute("user") == null){
+            model.addAttribute("user", new InternalUser());
+            return "login";
+        }
+        InternalUser user = (InternalUser) session.getAttribute("user");
+
+        Collaboration collaboration = collaborationDao.getCollaboration(idOffer, idRequest);
+        Offer offer = offerDao.getOffer(idOffer);
+        Request request = requestDao.getRequest(idRequest);
+        if (!offer.getUsername().equals(user.getUsername()) && !request.getUsername().equals(user.getUsername()))
+            throw new SkillSharingException("You are not allowed to update this collaboration", "NotAllowed", "/");
+        model.addAttribute("collaboration", collaboration);
+        List<String> assessments = new LinkedList<>();
+        for(AssessmentScore assessment : AssessmentScore.values())
+            assessments.add(assessment.getId());
+        model.addAttribute("assessments", assessments);
+        List<String> states = new LinkedList<>();
+        for(CollaborationState state : CollaborationState.values())
+            states.add(state.getId());
+        model.addAttribute("states", states);
+        return "collaboration/update";
+    }
+
+    @PostMapping(value = "/update")
+    public String processUpdateSubmit(@ModelAttribute("collaboration") Collaboration collaboration,
+                                      BindingResult bindingResult) {
+        validator.validate(collaboration, bindingResult);
+        if (bindingResult.hasErrors()) return "collaboration/update";
+        collaborationDao.updateCollaboration(collaboration);
+        return "redirect:list/";
+    }
+}
---
+++ src/main/java/com/aams/skillsharing/controller/OfferController.java
@@ -0,0 +1,110 @@
+package com.aams.skillsharing.controller;
+
+import com.aams.skillsharing.model.InternalUser;
+import com.aams.skillsharing.model.Offer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+@Controller
+@RequestMapping("/offer")
+public class OfferController extends RoleController{
+    private OfferDao offerDao;
+    private static final OfferValidator validator = new OfferValidator();
+
+    @Autowired
+    public void setOfferDao(OfferDao offerDao) {
+        this.offerDao = offerDao;
+    }
+
+    @RequestMapping("/list")
+    public String listOffers(Model model) {
+        List<Offer> offers = offerDao.getOffers();
+
+        model.addAttribute("offers", offers);
+        return "offer/list";
+    }
+
+    @RequestMapping("/list/{username}")
+    public String listOffersStudent(HttpSession session, Model model, @PathVariable String username) {
+        if (session.getAttribute("user") == null){
+            model.addAttribute("user", new InternalUser());
+            return "login";
+        }
+
+        model.addAttribute("offers", offerDao.getOffersStudent(username));
+        model.addAttribute("student", username);
+        return "offer/list";
+    }
+
+    @RequestMapping("/list/{name}")
+    public String listOffersSkill(Model model, @PathVariable String name) {
+        List<Offer> offers = offerDao.getOffersSkill(name);
+
+        model.addAttribute("offer", offers);
+        model.addAttribute("skill", name);
+        return "offer/list";
+    }
+
+    @RequestMapping(value = "/add/{name}")
+    public String addOffer(HttpSession session, Model model, @PathVariable String name) {
+        if (session.getAttribute("user") == null){
+            model.addAttribute("user", new InternalUser());
+            return "login";
+        }
+        InternalUser user = (InternalUser) session.getAttribute("user");
+
+        Offer offer = new Offer();
+        offer.setUsername(user.getUsername());
+        offer.setName(name);
+        model.addAttribute("offer", offer);
+        return "offer/add";
+    }
+
+    @PostMapping(value = "/add")
+    public String processAddOffer(@ModelAttribute("offer") Offer offer,
+                                  BindingResult bindingResult) {
+        validator.validate(offer, bindingResult);
+        if (bindingResult.hasErrors()) {
+            return "offer/add";
+        }
+        try {
+            offerDao.addOffer(offer);
+        }
+        catch (DataAccessException e){
+            throw new SkillSharingException("Error accessing the database\n" + e.getMessage(),
+                    "ErrorAccessingDatabase", "/");
+        }
+        return "redirect:list/";
+    }
+
+    @GetMapping(value = "/update/{id}")
+    public String updateOffer(HttpSession session, Model model, @PathVariable int id) {
+        if (session.getAttribute("user") == null){
+            model.addAttribute("user", new InternalUser());
+            return "login";
+        }
+        InternalUser user = (InternalUser) session.getAttribute("user");
+
+        Offer offer = offerDao.getOffer(id);
+        if (!offer.getUsername().equals(user.getUsername()))
+            throw new SkillSharingException("You are not allowed to update this offer", "NotAllowed", "/");
+        model.addAttribute("offer", offer);
+        return "offer/update";
+    }
+
+    @PostMapping(value = "/update")
+    public String processUpdateSubmit(@ModelAttribute("offer") Offer offer,
+                                      BindingResult bindingResult) {
+        validator.validate(offer, bindingResult);
+        if (bindingResult.hasErrors()) return "offer/update";
+        offerDao.updateOffer(offer);
+        return "redirect:list/";
+    }
+}
