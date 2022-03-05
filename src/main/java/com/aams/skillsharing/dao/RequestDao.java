b7e1d7d811e182aaf2a2ed1a15b5b9314e160ff5
AHMEUJI <al375879@uji.es>
2022-03-05 12:13:42 +0100

    DAOs implementados

---
+++ src/main/java/com/aams/skillsharing/dao/RequestDao.java
@@ -0,0 +1,99 @@
+package com.aams.skillsharing.dao;
+
+import com.aams.skillsharing.model.Request;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class RequestDao {
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public void setDataSource(DataSource ds) {
+        jdbcTemplate = new JdbcTemplate(ds);
+    }
+
+    public void addRequest(Request request) throws DuplicateKeyException {
+        jdbcTemplate.update("INSERT INTO request(name, username, start_date, finish_date, description) VALUES (?,?,?,?,?)",
+                request.getName(),
+                request.getUsername(),
+                request.getStartDate(),
+                request.getFinishDate(),
+                request.getDescription()
+        );
+    }
+
+    public void deleteRequest(Request request) {
+        jdbcTemplate.update("DELETE FROM request WHERE id = ?",
+                request.getId()
+        );
+    }
+
+    public void deleteRequest(int id){
+        jdbcTemplate.update("DELETE FROM request WHERE id = ?",
+                id
+        );
+    }
+
+    public void updateRequest(Request request) {
+        jdbcTemplate.update("UPDATE request SET id = ?, name = ?, username = ?, start_date = ?, finish_date = ?, description = ? WHERE id = ?",
+                request.getId(),
+                request.getName(),
+                request.getUsername(),
+                request.getStartDate(),
+                request.getFinishDate(),
+                request.getDescription(),
+                request.getId()
+        );
+    }
+
+    public Request getRequest(int id){
+        try {
+            return jdbcTemplate.queryForObject("SELECT * FROM request WHERE id = ?",
+                    new RequestRowMapper(),
+                    id
+            );
+        } catch (EmptyResultDataAccessException e) {
+            return null;
+        }
+    }
+
+    public List<Request> getRequests(){
+        try {
+            return jdbcTemplate.query("SELECT * FROM request",
+                    new RequestRowMapper()
+            );
+        } catch (EmptyResultDataAccessException e) {
+            return new ArrayList<>();
+        }
+    }
+
+    public List<Request> getRequestsStudent(String username){
+        try {
+            return jdbcTemplate.query("SELECT * FROM request WHERE username = ?",
+                    new RequestRowMapper(),
+                    username
+            );
+        } catch (EmptyResultDataAccessException e) {
+            return new ArrayList<>();
+        }
+    }
+
+    public List<Request> getRequestsSkill(String name){
+        try {
+            return jdbcTemplate.query("SELECT * FROM request WHERE name = ?",
+                    new RequestRowMapper(),
+                    name
+            );
+        } catch (EmptyResultDataAccessException e) {
+            return new ArrayList<>();
+        }
+    }
+}
